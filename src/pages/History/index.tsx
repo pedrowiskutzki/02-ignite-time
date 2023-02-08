78d12689016e8cde7a22ba0ad90b4212f724f82a
Pedro Wiskutzki <pedro.wiskutzki@neki-it.com.br>
2023-02-08 10:58:17 -0300

    Refactor(Context): Implement Context

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react'
+import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <HistoryContainer>
+      <pre>{JSON.stringify(cycles, null, 2)}</pre>
       <h1>History</h1>
       <HistoryList>
         <table>
